64c45d7a9aeecb0033c41cd01ae7beeb6274b9b0
tiosatria <35748586+tiosatria@users.noreply.github.com>
2025-10-29 06:20:50 +0700

    stable as fuck boi

--- bigdata/middlewares.py
+++ bigdata/middlewares.py
@@ -14,7 +14,11 @@ class ProxyMiddleware:
             request.meta['proxy'] = 'http://changeme:changeme@127.0.0.1:1234'
         elif request.meta.get('playwright', False):
             if request.meta.get('use_proxy', False):
-                request.meta['playwright_context_kwargs'] = {'proxy': {'server': 'http://p.webshare.io:9999'}}
+                request.meta['playwright_context_kwargs'] = {'proxy':
+                                                                 {'server': 'http://p.webshare.io:80',
+                                                                       'username': 'icpjabta-rotate',
+                                                                       'password': 'v3cylfcqz2p5'}
+                                                             }
         elif request.meta.get('use_proxy', False):
             request.meta['proxy'] = 'http://icpjabta-rotate:v3cylfcqz2p5@p.webshare.io:80'
 
